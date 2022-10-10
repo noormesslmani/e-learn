6e61fe20a0a6c76261f92ae7fabec9cdfb47b2e8
noormesslmani <noor.messlmani@gmail.com>
2022-10-11 00:42:54 +0300

    frontend: created no courses message

--- e-learn-frontend/src/components/courses.js
+++ e-learn-frontend/src/components/courses.js
@@ -5,29 +5,23 @@ import EnrolledCourseCard from './enrolledCourseCard';
 const baseURL='http://127.0.0.1:8000/api/v1/';
 
 export default function Courses() {
-    const [nocourses, setNocourses] = useState(false);
     const [courses, setCourses] = useState([]);
     useEffect(() => {
-        setNocourses(false);
         let config = {
             headers: { Authorization: `Bearer ${localStorage.getItem("token")}`},
         };
         let res = axios.get(baseURL+"getenrolledcourses",config)
         .then(function (response) {
-            if(response.data.courses.length==0){
-                setNocourses(true);
-            }
-            console.log(response.data.courses)
             setCourses(response.data.courses)
         })
         .catch(function (error) {
             console.log(error);
         });
     }, []);   
-    console.log(localStorage) 
+
     const noCoursesMessage = () => {
         return (
-          <h2 className="messsage" style={{display: nocourses ? 'block' : 'none',}}>
+          <h2 className="messsage" >
             No courses to display :(
           </h2>
         );
@@ -36,8 +30,12 @@ export default function Courses() {
         <div className='suggested-courses' >
             <h1>Your Courses</h1>
             <div className='displayed-courses'>
-                {courses.map((id)=><EnrolledCourseCard id={id} />)} 
-                <div>{noCoursesMessage()}</div>
+                {
+                    courses.length>0? (
+                    <div className='displayed-courses'>
+                    {courses.map((id)=><EnrolledCourseCard id={id} />)} 
+                    </div>): <div>{noCoursesMessage()}</div>
+                }
             </div>
         </div>      
     );
