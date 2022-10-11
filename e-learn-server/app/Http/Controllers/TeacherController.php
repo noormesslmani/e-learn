bb9a27bf09b6097e5203fcebc1a5b185ac92f3c2
noormesslmani <noor.messlmani@gmail.com>
2022-10-11 20:55:27 +0300

    Backend: modified createAssignment function request parameters

--- e-learn-server/app/Http/Controllers/TeacherController.php
+++ e-learn-server/app/Http/Controllers/TeacherController.php
@@ -16,7 +16,6 @@ class TeacherController extends Controller
 {
     public function createAssignment(Request $request){
         $validator = Validator::make($request->all(), [
-            "name"=>"required",
             "description"=>"required",
         ]);
         if ($validator->fails()) {
@@ -24,16 +23,10 @@ class TeacherController extends Controller
                 "message" => "Validation failed"
             ]);
         }
-        try{
-            $course_id=Course::select('id')->where('name',$request->name)->get()[0]['_id'];
-        }
-        catch(\Exception $e){
-            return response()->json(["result" => $e->getMessage()], 404); 
-        }
         Assignment::insert([
             'description' =>$request->description,
             'created_at' => date('d-m-y h:i:s'),
-            'course_id'=> $course_id,
+            'course_id'=> $request->course_id,
             'teacher_id'=> Auth::user()->id
         ]);
         return response()->json(["result" => "ok"], 201); 
