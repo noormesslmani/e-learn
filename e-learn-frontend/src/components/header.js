1f8ee7564d92b74f1b41bb543ff8d4fe4b515dd2
noormesslmani <noor.messlmani@gmail.com>
2022-10-11 16:55:39 +0300

    Frontend: implemented get assignments api in course details page

--- e-learn-frontend/src/components/header.js
+++ e-learn-frontend/src/components/header.js
@@ -5,10 +5,9 @@ export default function Header() {
     return (
         <div className='header'>
     
-            <Link className='course-links' >View assignments |</Link>
-            <Link className='course-links' >Add assignment |</Link>
-            <Link className='course-links' >View students |</Link>
-            <Link className='course-links' >Enrole student</Link>
+            <Link className='course-links' >View assignments </Link>
+            <Link className='course-links' >Add assignment </Link>
+            <Link className='course-links' >Enroll student</Link>
             
         </div>      
     );
--- e-learn-frontend/src/components/pages/CourseDetails.js
+++ e-learn-frontend/src/components/pages/CourseDetails.js
@@ -1,11 +1,26 @@
 import '../../Student.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../navbarInstructor';
 import { useLocation } from 'react-router-dom';
 import Header from '../header';
+import axios from 'axios';
+const baseURL='http://127.0.0.1:8000/api/v1/';
 function CourseDetails() {
     const { state } = useLocation();
-    console.log(state)
+    console.log(state.clickedCourse._id)
+    useEffect(() => {
+        let config = {
+            headers: { Authorization: `Bearer ${localStorage.getItem("token")}`},
+        };
+        let payload= {course_id: state.clickedCourse._id}
+        let res = axios.get(baseURL+"getassignments",payload,config)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }, []); 
     return (
         <>
             <Navbar/>
