8a61b9d16cc9620554b5c61e16f3cbc06224a65f
noormesslmani <noor.messlmani@gmail.com>
2022-10-12 01:11:33 +0300

    Frontend: added some styling to assignments card

--- e-learn-frontend/src/components/instructor/assignmentInstructor.js
+++ e-learn-frontend/src/components/instructor/assignmentInstructor.js
@@ -4,10 +4,9 @@ export default function Assignment({assignment, state}) {
     console.log(assignment);
     console.log(state.clickedCourse);
     return(
-        <div className='course-card'>
-            <div className='course-name' >{state.clickedCourse.name}</div>
+        <div className='instructor-assignments-card'>
+            <h4 className='course-name' >{state.clickedCourse.name}</h4>
             <div className='assignment-description'><span className='requirements'>Requirements:</span> {assignment.description}</div>
-
         </div> 
         )
     }
