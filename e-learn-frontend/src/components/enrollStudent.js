f0d13b5406c504234b4cbd4aa79c409079e0debc
noormesslmani <noor.messlmani@gmail.com>
2022-10-11 22:09:42 +0300

    Frontend: implemented enroll student in a course

---
+++ e-learn-frontend/src/components/enrollStudent.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import '../Student.css'
+
+export default function EnrollStudent({handleCancelEnroll,handleSubmitEnroll,state, setUsername, nonexistant, invalidType}) {
+    const handleChange=(e)=>{
+        setUsername(e.target.value)
+    }
+    return(
+        <div className='submit-modal'>
+            <div className='content'>
+                <h2>Enroll Student in the course</h2>
+                <h4>Course Name: {state.clickedCourse.name}</h4>
+                <label for="username" >Username</label>
+                <input placeholder='Student Username' onChange={handleChange} type="text" className="student-username" name="username" />
+                <div className="assignment-btns">
+                    <button className='submit-btn' onClick={handleSubmitEnroll}>Submit</button>
+                    <button className="cancel-btn" onClick={handleCancelEnroll}>Cancel</button>
+                </div>
+                {nonexistant? (<div className='message'>User does not exist</div>):<></> }
+                {invalidType? (<div className='message'>Invalid user type</div>):<></> }
+            </div>
+        </div> 
+    )
+}
--- e-learn-frontend/src/components/pages/CourseDetails.js
+++ e-learn-frontend/src/components/pages/CourseDetails.js
@@ -5,13 +5,17 @@ import { useLocation} from 'react-router-dom';
 import Assignment from '../assignmentInstructor';
 import axios from 'axios';
 import CreateAssignment from '../createAssignment';
+import EnrollStudent from '../enrollStudent';
 const baseURL='http://127.0.0.1:8000/api/v1/';
 function CourseDetails() {
     const [assignments, setAssignments] = useState([]);
     const [assignmentModal, setAssignmentModal] = useState(false);
     const [enrollModal, setEnrollModal] = useState(false);
     const [description, setDescription] = useState('');
+    const [username, setUsername] = useState('');
     const [assignmentCreated, setAssignmentCreation] = useState(false);
+    const [nonexistant, setNonexistant] = useState(false);
+    const [invalidType, setInvalidType]= useState(false);
     const { state } = useLocation();
     useEffect(() => {
         let config = {
@@ -26,11 +30,14 @@ function CourseDetails() {
             console.log(error);
         });
     }, [assignmentCreated]); 
+
     const handleClickAssignment=(e)=>{
         setAssignmentModal(true);
     }
     const handleClickEnroll=(e)=>{
-        console.log(e.target);
+        setEnrollModal(true);
+        setNonexistant(false);
+        setInvalidType(false);
     }
     const handleCancelAssignment=(e)=>{
         e.preventDefault()
@@ -38,9 +45,21 @@ function CourseDetails() {
     }
     const handleSubmitAssignment=(e)=>{
         setAssignmentCreation(false);
-        e.preventDefault()
+        e.preventDefault();
         if(description!=''){
             createNewAssignment();
+        }
+    }
+    const handleCancelEnroll=(e)=>{
+        e.preventDefault();
+        setEnrollModal(false);
+    }
+    const handleSubmitEnroll=(e)=>{
+        e.preventDefault();
+        setInvalidType(false);
+        setNonexistant(false);
+        if(username!=''){
+            enrollStudent();
         }
     }
     function createNewAssignment(){
@@ -53,6 +72,28 @@ function CourseDetails() {
             if(response.data.result=='ok'){
                 setAssignmentModal(false);
                 setAssignmentCreation(true);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+    function enrollStudent(){
+        let config = {
+            headers: { Authorization: `Bearer ${localStorage.getItem("token")}`},
+        };
+        let payload= {course_id: state.clickedCourse._id, username: username}
+        let res = axios.post(baseURL+"enrollstudent",payload,config)
+        .then(function (response) {
+            console.log(response.data.result)
+            if(response.data.result=='ok'){
+                setEnrollModal(false)
+            }
+            else if(response.data.result=='user does not exist'){
+                setNonexistant(true);
+            }
+            else if(response.data.result=='invalid user type'){
+                setInvalidType(true)
             }
         })
         .catch(function (error) {
@@ -75,6 +116,7 @@ function CourseDetails() {
             }
             {assignmentModal? (<CreateAssignment setDescription={setDescription} state={state} handleCancelAssignment={handleCancelAssignment}
             handleSubmitAssignment={handleSubmitAssignment} />):<></>}
+            {enrollModal? (<EnrollStudent state={state} handleCancelEnroll={handleCancelEnroll} handleSubmitEnroll={handleSubmitEnroll} setUsername={setUsername} nonexistant={nonexistant} invalidType={invalidType} />):<></>}
         </>
     );
 }
