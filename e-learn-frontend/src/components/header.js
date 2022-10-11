36bf25cd563b16b4f3b0136f5749c085c49553cd
noormesslmani <noor.messlmani@gmail.com>
2022-10-11 16:36:53 +0300

    Frontend: added header component to course details page

---
+++ e-learn-frontend/src/components/header.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import '../App.css'
+import { Link } from 'react-router-dom';
+export default function Header() {
+    return (
+        <div className='header'>
+    
+            <Link className='course-links' >View assignments |</Link>
+            <Link className='course-links' >Add assignment |</Link>
+            <Link className='course-links' >View students |</Link>
+            <Link className='course-links' >Enrole student</Link>
+            
+        </div>      
+    );
+}
--- e-learn-frontend/src/components/pages/CourseDetails.js
+++ e-learn-frontend/src/components/pages/CourseDetails.js
@@ -2,12 +2,14 @@ import '../../Student.css';
 import React, { useState } from 'react';
 import Navbar from '../navbarInstructor';
 import { useLocation } from 'react-router-dom';
+import Header from '../header';
 function CourseDetails() {
     const { state } = useLocation();
     console.log(state)
     return (
         <>
             <Navbar/>
+            <Header/>
         </>
     );
 }
